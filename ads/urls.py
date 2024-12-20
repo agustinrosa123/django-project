a43231cbc28189a665493807c31044c359f305d8
Agustin <agustinpr@192.168.1.9>
2024-12-20 02:59:07 -0300

    Agregados meta tags de dj4e y enlace de login en base.html

--- ads/urls.py
+++ ads/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views  # Importa las vistas de autenticación de Django
+from . import views  # Asegúrate de importar views para usar las vistas propias
 
 urlpatterns = [
     path('ads/', views.ads_list, name='ads_list'),  # Ruta para la lista de anuncios
