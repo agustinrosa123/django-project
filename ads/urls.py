7337f00ef9d09c281f0b08c639efa86264984db7
Agustin <agustinpr@192.168.1.9>
2024-12-20 02:50:07 -0300

    Agregado enlace de login en base.html y actualizado urls.py

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views  # Importa las vistas de autenticación de Django
 
 urlpatterns = [
-    path('ads/', views.ads_list, name='ads_list'),  # Asegúrate de que 'views.ads_list' esté correcto
+    path('ads/', views.ads_list, name='ads_list'),  # Ruta para la lista de anuncios
+    path('login/', auth_views.LoginView.as_view(), name='login'),  # Ruta para el login
 ]
